90fe65a9dc6004f3e599a3088e9607c381e624e5
Mark Alber <markalber59@gmail.com>
2025-06-22 14:56:48 +0300

    added security in dashboard route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,20 +1,19 @@
 "use client";
 
 import { useUser } from "@/components/UserProvider";
-import { Loader2 } from "lucide-react";
 import { useSidebar } from "@/context/SidebarContext";
 import Overview_page from "@/components/ui/Overview_page";
+import { useRouter } from "next/router";
 
 export default function Dashboard() {
   const { selectedTab} = useSidebar();
+  const router = useRouter()
 
   const { user } = useUser();
 
   if (!user)
     return (
-      <div className="flex items-center justify-center h-screen">
-        <Loader2 className="animate-spin h-10 w-10 text-blue-500" />
-      </div>
+      router.push("/login")
     );
 
   return (
